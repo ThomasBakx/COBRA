fadf978255e8cd86e4577afdd4458b842cdc88dc
PabloMosUU <p.j.mosteiroromero@uu.nl>
2025-03-04 09:48:38 +0100

    unnecessary parentheses

--- Utils/extrapolate_pk.py
+++ Utils/extrapolate_pk.py
@@ -20,9 +20,9 @@ def extrapolate_pk(k_pk:np.ndarray):
         n_extrap_low = np.ceil(50*np.log10(kmin/1e-5)).astype(int)
         n_extrap_high = np.ceil(50*np.log10(1e2/kmax)).astype(int)
         k_extrap_low = np.logspace(-5, np.log10(kmin), n_extrap_low)
-        pk_extrap_low = pk_in[0] * (k_extrap_low / kmin) ** (ns_fid)
+        pk_extrap_low = pk_in[0] * (k_extrap_low / kmin) ** ns_fid
         k_extrap_high = np.logspace(np.log10(kmax), 2, n_extrap_high)
-        pk_extrap_high = pk_in[-1] * np.exp(- (((k_extrap_high) - kmax) / decay) ** 2)
+        pk_extrap_high = pk_in[-1] * np.exp(- ((k_extrap_high - kmax) / decay) ** 2)
                
         k_extrap = np.concatenate([k_extrap_low[:-1], k_in, k_extrap_high[1:]])
         pk_extrap = np.concatenate([pk_extrap_low[:-1], pk_in, pk_extrap_high[1:]])
